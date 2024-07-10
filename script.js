80f5c073903bb5dd5a1aeb814aa091c2c902cab7
Gabriel03333 <gabrielbulandeambrosio03@gmail.com>
2024-07-10 16:40:45 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function learnMore() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+});
